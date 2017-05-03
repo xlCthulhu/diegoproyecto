bf919a16dfe12990a45e1ec7490190ebc5b762d3
sena <sena@sena-MS-7817>
2017-05-03 09:06:00 -0500

    v.08

--- src/javaapplication8/Usuario.java
+++ src/javaapplication8/Usuario.java
@@ -10,5 +10,50 @@ package javaapplication8;
  * @author sena
  */
 public class Usuario {
+    private int idUsuario;
+    public void setIdUsuario (int idUsuarioP)
+    {
+        this.idUsuario=idUsuarioP;
+    }
+    public int getIdUsuario()
+    {
+        return this.idUsuario;
+      
+    }
+    
+  
+    private String nombre;
+    public void setNombre (String nombreP)
+    {
+        this.nombre=nombreP;
+        
+    }
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    
+     private String apellidos;
+    public void setApellidos (String apellidosP)
+    {
+        this.apellidos=apellidosP;
+        
+    }
+    public String getApellidos()
+    {
+        return this.apellidos;
+    }
+    
+     private int telefono;
+    public void setTelefono (int telefonoP)
+    {
+        this.idUsuario=telefonoP;
+    }
+    public int getTelefono()
+    {
+        return this.telefono;
+      
+    }
+    
     
 }
